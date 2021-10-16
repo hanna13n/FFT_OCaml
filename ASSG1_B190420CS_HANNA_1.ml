ce1d23ce428b5df33dfca0184316edb38ffa7988
hanna13n <hanna.nechikkadan@gmail.com>
2021-10-16 20:06:49 +0530

    Commented the steps and renamed for submission

--- ASSG1_B190420CS_HANNA_1.ml
+++ ASSG1_B190420CS_HANNA_1.ml
@@ -24,40 +24,47 @@ let rec fft a n w =
 
 let multiply_pol () = 
 
+   (* Reading the inputs *)
    print_endline "Degree of first polynomial (int) :";
    let n1 = read_int() in 
    print_endline "Degree of second polynomial (int) :";
    let n2 = read_int() in 
 
+   (* Evaluating the degree of the product polynomial *)
    let n = nextpowof2 (n1 + n2) in
 
    let a1 = Array.make n { re = 0.0; im = 0.0 } in
    let b1 = Array.make n { re = 0.0; im = 0.0 } in
 
+   (* First polynomial *)
    print_endline "Coefficient vector of 1st polynomial (float) :";
    for i = 0 to n1 do 
       a1.(i) <- {re = read_float(); im = 0.0 };
    done;
 
-   
+   (* Second polynomial *)
    print_endline "Coefficient vector of 2nd polynomial (float) :";
    for i = 0 to n2 do 
       b1.(i) <- {re = read_float(); im = 0.0 };
    done;
     
-   
+   (* Evaluating omega w *)
    let theta = 2.0 *. Float.pi /. float_of_int n in
    let w = {re = cos theta; im = sin theta } in 
 
+   (* FFT of polynomial gives Value Repn of each polynomial *)
    let a2 = fft a1 n w in
    let b2 = fft b1 n w in 
    
+   (* Evaluating Product polynomial in Value Repn *)
    let c1 = Array.init n (fun i -> mul a2.(i) b2.(i)) in
 
+   (* Interpolation : Value -> Coeff *)
    let win = conj w in 
    let c2 = fft c1 n win in
    let d = Array.map (fun c -> { re = c.re /. (float_of_int n); im = 0. }) c2 in
 
+   (* Printing the Product *)
    print_endline "Coefficient vector of product :" ;
    Array.iteri (fun i di -> if i < (n1 + n2 + 1) then Printf.printf "%.1f " di.re) d;
    print_newline ();
